a34bad33a0db5687f9943f29d0d98ae8396f919d
RodoGitHub <r.palacios@itecriocuarto.org.ar>
2025-02-23 22:44:45 -0300

    mas cambios

---
+++ schemas.py
@@ -0,0 +1,35 @@
+from app import ma
+
+from marshmallow import validates, ValidationError
+
+from models import User, Marca, Modelo
+
+class UserSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = User
+
+    id = ma.auto_field()
+    username = ma.auto_field()
+    password_hash = ma.auto_field()
+    is_admin = ma.auto_field()
+
+class UserMinimalSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = User
+
+    username = ma.auto_field()
+
+class MarcaSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Marca
+        
+    id = ma.auto_field()
+    nombre = ma.auto_field()
+
+class ModeloSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Modelo
+        
+    id = ma.auto_field()
+    nombre = ma.auto_field()
+    id_marca = ma.auto_field(required=True)
---
+++ views/__int__.py
@@ -0,0 +1,15 @@
+from .auth_views import auth_bp
+from .balanza_views import balanza_bp
+from .clientes_views import clientes_bp
+from .facturacion_views import facturacion_bp
+from .productos_views import productos_bp
+
+
+
+def register_bp(app):
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(balanza_bp)
+    app.register_blueprint(clientes_bp)
+    app.register_blueprint(facturacion_bp)
+    app.register_blueprint(productos_bp)
+
