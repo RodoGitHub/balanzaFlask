b1d06811aac67266d8e88c958ee79fe8e3da6716
Jonathan Desplats <124144015+joninet@users.noreply.github.com>
2025-02-27 09:16:17 +0000

    ok

--- insertardb.py
+++ insertardb.py
@@ -1,17 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from models import db, Usuario, Persona  # Importamos db, Usuario y Persona desde models
+from models import db, Usuario, Persona 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)  # Inicializar db con la app
+db.init_app(app) 
 
 def insertar_usuarios():
     with app.app_context():
-        db.create_all()  # Asegurar que las tablas existen
-        # Insertar usuarios
+        db.create_all()
         usuarios = [
             Usuario(nombre_usuario='admin', password='1234', rol_id=1, persona_id=1),
         ]
